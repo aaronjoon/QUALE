75375b3338f4a30ed3316f482ecbcc95d309dd72
Yogev Angelovici <angeloviciy@gmail.com>
2024-02-03 16:35:40 -0500

    circuit input finally working :)
    Edited circuit to make a full circuit and accept statevector input

--- main.py
+++ main.py
@@ -13,63 +13,98 @@ discriminator1 = pcvl.Circuit(4,'discriminator1')
 generator2 = pcvl.Circuit(4,"generator2")
 discriminator2 = pcvl.Circuit(4,'discriminator2')
 
-def generate_generator(generator):
-    generator.add(1, PS(phi=pcvl.P('phi.g1.1')))
-    generator.add(2, PS(phi=pcvl.P('phi.g1.2')))
-    generator.add(3, PS(phi=pcvl.P('phi.g1.3')))
+full_circuit = pcvl.Circuit(8, "Full Circuit")
+
+phi=np.pi/2
+
+def generate_generator(generator, top_bottom):
+    generator.add(1, PS(phi=pcvl.P(f'phi.g1.1.{top_bottom}')))
+    generator.add(2, PS(phi=pcvl.P(f'phi.g1.2.{top_bottom}')))
+    generator.add(3, PS(phi=pcvl.P(f'phi.g1.3.{top_bottom}')))
     generator.add(1, BS())
-    generator.add(1, PS(phi=pcvl.P('phi.g1.4')))
+    generator.add(1, PS(phi=pcvl.P(f'phi.g1.4.{top_bottom}')))
     generator.add(1, BS())
     generator.add(0, BS())
     generator.add(2, BS())
-    generator.add(0, PS(phi=pcvl.P('phi.g1.5')))
-    generator.add(2, PS(phi=pcvl.P('phi.g1.6')))
+    generator.add(0, PS(phi=pcvl.P(f'phi.g1.5.{top_bottom}')))
+    generator.add(2, PS(phi=pcvl.P(f'phi.g1.6.{top_bottom}')))
     generator.add(0, BS())
     generator.add(2, BS())
-    generator.add(1, PS(phi=pcvl.P('phi.g1.7')))
+    generator.add(1, PS(phi=pcvl.P(f'phi.g1.7.{top_bottom}')))
     generator.add(1, BS())
-    generator.add(1, PS(phi=pcvl.P('phi.g1.8')))
+    generator.add(1, PS(phi=pcvl.P(f'phi.g1.8.{top_bottom}')))
     generator.add(1, BS())
 
-    generator.add(0, PS(phi=pcvl.P('phi.g1.9')))
-    generator.add(2, PS(phi=pcvl.P('phi.g1.10')))
+    generator.add(0, PS(phi=pcvl.P(f'phi.g1.9.{top_bottom}')))
+    generator.add(2, PS(phi=pcvl.P(f'phi.g1.10.{top_bottom}')))
     generator.add(0, BS())
     generator.add(2, BS())
     
-    generator.add(0, PS(phi=pcvl.P('phi.g1.11')))
-    generator.add(2, PS(phi=pcvl.P('phi.g1.12')))
+    generator.add(0, PS(phi=pcvl.P(f'phi.g1.11.{top_bottom}')))
+    generator.add(2, PS(phi=pcvl.P(f'phi.g1.12.{top_bottom}')))
     generator.add(0, BS())
     generator.add(2, BS())
 
-    generator.add(0, PS(phi=pcvl.P('phi.g1.13')))
-    generator.add(1, PS(phi=pcvl.P('phi.g1.14')))
-    generator.add(2, PS(phi=pcvl.P('phi.g1.15')))
+    generator.add(0, PS(phi=pcvl.P(f'phi.g1.13.{top_bottom}')))
+    generator.add(1, PS(phi=pcvl.P(f'phi.g1.14.{top_bottom}')))
+    generator.add(2, PS(phi=pcvl.P(f'phi.g1.15.{top_bottom}')))
 
     return generator
 
-def generate_discriminator(discriminator):
-    discriminator.add(0, PS(phi=pcvl.P('phi.d1.1')))
-    discriminator.add(1, PS(phi=pcvl.P('phi.d1.2')))
-    discriminator.add(2, PS(phi=pcvl.P('phi.d1.3')))
+def generate_discriminator(discriminator, top_bottom):
+    discriminator.add(0, PS(phi=pcvl.P(f'phi.d1.1.{top_bottom}')))
+    discriminator.add(1, PS(phi=pcvl.P(f'phi.d1.2.{top_bottom}')))
+    discriminator.add(2, PS(phi=pcvl.P(f'phi.d1.3.{top_bottom}')))
     discriminator.add(0, BS())
     discriminator.add(2, BS())
-    discriminator.add(0, PS(phi=pcvl.P('phi.d1.5')))
-    discriminator.add(2, PS(phi=pcvl.P('phi.d1.6')))
+    discriminator.add(0, PS(phi=pcvl.P(f'phi.d1.5.{top_bottom}')))
+    discriminator.add(2, PS(phi=pcvl.P(f'phi.d1.6.{top_bottom}')))
     discriminator.add(0, BS())
     discriminator.add(2, BS())
-    discriminator.add(1, PS(phi=pcvl.P('phi.d1.7')))
+    discriminator.add(1, PS(phi=pcvl.P(f'phi.d1.7.{top_bottom}')))
     discriminator.add(1, BS())
-    discriminator.add(1, PS(phi=pcvl.P('phi.d1.8')))
+    discriminator.add(1, PS(phi=pcvl.P(f'phi.d1.8.{top_bottom}')))
     
     return discriminator
 
+def generate_input_state():
+    state1 = pcvl.StateVector([1, 0, 0, 0, 1, 0, 0, 0])
+    state2 = pcvl.StateVector([0, 1, 0, 0, 0, 1, 0, 0])
+    state3 = pcvl.StateVector([0, 0, 1, 0, 0, 0, 1, 0])
+    state4 = pcvl.StateVector([0, 0, 0, 1, 0, 0, 0, 1])
+
+
+    input_state = state1 + state2 + state3 + state4
+
+    return input_state
+
+def possible_output_states():
+
+    return [pcvl.BasicState([1, 0, 0, 0, 1, 0, 0 , 0]),
+            pcvl.BasicState([1, 0, 0, 0, 0, 1, 0 , 0]),
+            pcvl.BasicState([1, 0, 0, 0, 0, 0, 1 , 0]),
+            pcvl.BasicState([1, 0, 0, 0, 0, 0, 0 , 1]),
+            pcvl.BasicState([0, 1, 0, 0, 1, 0, 0 , 0]),
+            pcvl.BasicState([0, 1, 0, 0, 0, 1, 0 , 0]),
+            pcvl.BasicState([0, 1, 0, 0, 0, 0, 1 , 0]),
+            pcvl.BasicState([0, 1, 0, 0, 0, 0, 0 , 1]),
+            pcvl.BasicState([0, 0, 1, 0, 1, 0, 0 , 0]),
+            pcvl.BasicState([0, 0, 1, 0, 0, 1, 0 , 0]),
+            pcvl.BasicState([0, 0, 1, 0, 0, 0, 1 , 0]),
+            pcvl.BasicState([0, 0, 1, 0, 0, 0, 0 , 1]),
+            pcvl.BasicState([0, 0, 0, 1, 1, 0, 0 , 1]),
+            pcvl.BasicState([0, 0, 0, 1, 0, 1, 0 , 0]),
+            pcvl.BasicState([0, 0, 0, 1, 0, 0, 1 , 0]),
+            pcvl.BasicState([0, 0, 0, 1, 0, 0, 0 , 1])]
+            
+
 def main():
 
-    generate_generator(generator1)
-    generate_discriminator(discriminator1)
+    generate_generator(generator1, 'top')
+    generate_discriminator(discriminator1, 'top')
 
-    generate_generator(generator2)
-    generate_discriminator(discriminator2)
+    generate_generator(generator2, 'bottom')
+    generate_discriminator(discriminator2, 'bottom')
     
 
     main_circuit.add(0, generator1)
@@ -78,11 +113,23 @@ def main():
     secondary_circuit.add(0, generator2)
     secondary_circuit.add(0, discriminator2)
 
+    full_circuit.add(0, main_circuit).add(4, secondary_circuit)
+
+    input_state = generate_input_state()
+    outputs = possible_output_states()
+
     #params=generator1.get_parameters()
     #print(params)
     #pcvl.pdisplay(discriminator1)
     pcvl.pdisplay(main_circuit) #,recursive=True)
     pcvl.pdisplay(secondary_circuit)
+    pcvl.pdisplay(full_circuit)
+
+    print(input_state)
+
+    p = pcvl.Processor("SLOS", full_circuit)
+    analysis = pcvl.algorithm.Analyzer(p, outputs)
+    pcvl.pdisplay(analysis)
 
 if __name__ == "__main__":
     main()
